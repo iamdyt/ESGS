afd95061b39b268c1481012694cdf6d3efd92f47
iamdyt <olanlokunlanre@gmai.com>
2019-10-13 07:12:54 -0700

    Update Diagnose me

--- controllers/Diagnosis.js
+++ controllers/Diagnosis.js
@@ -10,16 +10,27 @@ module.exports = {
     diagnose:(req,res)=>{
         const params = req.body.answer;
         if (params.length<4){
-            const text = "You do not have Gonorrhea";
+            const text = `
+            <p class='lead text-nurse-blue text-justify'>You do not show sign of<strong> GONORRHEA INFECTION</strong>
+            However, your stated symptoms may be as result of another STI (SEXUAL TRANSMITTED INFECTION) kindly meet <strong>A QUALIFIED PHYSICIAN</strong> with explanation of your <strong> SYMPTOMS. </strong> </p>
+        ` 
             res.render('diagnosis/result', {text});
         }
         else if (params.length < 7){
-            const text = "You show Signs of Early Gonorrhea";
+            const text = `
+                <p class='lead text-nurse-blue text-justify'>You show signs of <strong> EARLY GONORRHEA</strong>
+                ensure you arrange a meeting with your DOCTOR for further clinical diagnosis
+                You can also meet <strong>A QUALIFIED PHARMACIST</strong> with explanation of your <strong> SYMPTOMS. </strong> </p>
+            ` 
             res.render('diagnosis/result', {text});
         }
         
         else{
-            const text = "You have been infected with Gonorrea";
+            const text = `
+            <p class='lead text-nurse-blue text-justify'>You show signs of <strong> GONORRHEA INFECTION</strong>
+            ensure you arrange a meeting with your DOCTOR for further clinical diagnosis
+            You can also meet <strong>A QUALIFIED PHARMACIST</strong> with explanation of your <strong> SYMPTOMS. </strong> </p>
+        ` 
             res.render('diagnosis/result', {text});
         }
     }
